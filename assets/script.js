c690f152fc23ba13dcf41e4d638d542991d29a50
Eugenio Espinoza Garcia <eugenioespinozagarcia@Eugenios-MacBook-Pro.local>
2020-10-22 00:37:49 -0500

    CSS + Script Update

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+let questions = [
+    {
+        //Question #1 
+        question: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Language", "How to Make Laughs", "Home Tool Markup Language", "Hyperlinks and Text Markup Language"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        //Question #2
+        question: "Choose the correct HTML element to define emphasized text:",
+        choices: ["<i>", "<em>", "<italic>", "<emphasized>"],
+        answer: "<em>"
+    },
+    {
+        //Question #3
+        question: "How can you make a numbered list?",
+        choices: ["<ul>", "<list>", "<ol>", "<dl>"],
+        answer: "<ol>"
+    },
+    {
+        //Question #4
+        question: "Which HTML element is used to specify a footer for a document or section?",
+        choices: ["<bottom>", "<section>", "<footer>", "all of the above"],
+        answer: "<footer>"
+    },
+    {
+        //Question #5
+        question: "Choose the correct HTML element for the largest heading:",
+        choices: ["<h6>", "<head>", "<h1>", "<heading>"],
+        answer: "<h1>"
+    },
+
+];
